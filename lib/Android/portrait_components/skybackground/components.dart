2badbeb0cab61931f30cbe31f8e78d4648778f81
Remon Ahammad <blackshadowsoftwareltd@gmail.com>
2021-08-12 02:14:21 +0600

    portrait skybackground complate

--- lib/Android/portrait_components/skybackground/components.dart
+++ lib/Android/portrait_components/skybackground/components.dart
@@ -1,8 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:remonahammad/Android/constant/size_config.dart';
 
-Widget sky() => Container(height: 350,
-      child: Image.asset(
-        'assets/image/sky.png',
-        fit: BoxFit.cover,
-      ),
-    );
+/// SKY
+Widget sky() => Container(
+    height: 350, child: Image.asset('assets/image/sky.png', fit: BoxFit.cover));
+
+/// Grass 1
+Widget grass1() => Container(
+    height: 40,
+    child: Image.asset('assets/image/grass1.png', fit: BoxFit.fitHeight));
+Widget grass() => Container(
+    height: 40,
+    child: Image.asset('assets/image/grass.png', fit: BoxFit.fitHeight));
+Widget grass2() => Container(
+    height: 80,
+    child: Image.asset('assets/image/grass2.png', fit: BoxFit.fitHeight));
+Widget grass3() => Container(
+    height: 80,
+    child: Image.asset('assets/image/grass3.png', fit: BoxFit.fitHeight));
+Widget grass4() => Container(
+    height: 120,
+    child: Image.asset('assets/image/grass4.png', fit: BoxFit.fitHeight));
+Widget tree3() => Container(
+    height: 80,
+    child: Image.asset('assets/image/tree3.png', fit: BoxFit.fitHeight));
+Widget verySmallTreeR() => Container(
+    height: 60,
+    child: Image.asset('assets/image/verySmallTreeR.png', fit: BoxFit.fitHeight));
+Widget verySmallTree() => Container(
+    height: 60,
+    child: Image.asset('assets/image/verySmallTree.png', fit: BoxFit.fitHeight));
+Widget mount() => Container(
+   width:  ScreenSize.pWidth!+20,
+    child: Image.asset('assets/image/mount.png', fit: BoxFit.fitWidth));
+Widget birdDown() => Container(
+   height: 170,
+    child: Image.asset('assets/image/birdDown.png', fit: BoxFit.fitHeight));
+Widget birdRight() => Container(
+   height: 120,
+    child: Image.asset('assets/image/birdRight.png', fit: BoxFit.fitHeight));
+Widget birdLeft() => Container(
+   height: 80,
+    child: Image.asset('assets/image/birdLeft.png', fit: BoxFit.fitHeight));
+Widget extraLargeTree() => Container(
+   height: 80,
+    child: Image.asset('assets/image/extraLargeTree.png', fit: BoxFit.fitHeight));
+Widget bigTree2() => Container(
+   height:100,
+    child: Image.asset('assets/image/bigTree2.png', fit: BoxFit.fitHeight));
+Widget tree1() => Container(
+   height:90,
+    child: Image.asset('assets/image/tree1.png', fit: BoxFit.fitHeight));
+Widget f22(w) => Container(
+  width:w ,
+    child: Image.asset('assets/image/f22.png', fit: BoxFit.fitHeight));
+Widget moon() => Container(
+  width:20 ,
+    child: Image.asset('assets/image/moon.png', fit: BoxFit.fitHeight));
--- lib/Android/portrait_components/skybackground/portait_skybackground.dart
+++ lib/Android/portrait_components/skybackground/portait_skybackground.dart
@@ -10,12 +10,80 @@ class PortraitSkyBackground extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     value = context.watch<OrientationScrollProvider>().portScroll;
+    double top=value!<150?0:value!-150,right=value!<150?0:value!-150;
     return Container(
       height: 505,
       width: ScreenSize.pWidth,
       color: Colors.blue[100],
       child: Stack(
-        children: [Positioned(top: -value! * 0.05, child: sky())],
+        children: [
+          Positioned(top: -value! * 0.02, child: sky()),
+
+          /// moon
+          Positioned(
+              top: top*.4 + 40, right:right*.5+ 80, child: moon()),
+          /// bird
+          Positioned(
+              top: value! * .20 + 100, left: value! * .9, child: birdRight()),
+          /// f22
+          Positioned(
+              top:  150-value!*.45, right: 10+value!, child: f22(15+value!*.2)),
+          Positioned(
+              top: value! * .20 + 150, right: value! * .5, child: birdLeft()),
+
+          /// tree
+          Positioned(
+              top: -value! * .40 + 175, left: -5, child: extraLargeTree()),
+          /// tree
+          Positioned(
+              top: -value! * .40 + 190, right: 20, child: tree1()),
+
+          /// mount
+          Positioned(
+              top: -value! * .50 + 200, left: -2, right: -2, child: mount()),
+
+          /// tree
+          Positioned(top: -value! * .55 + 270, left:50, child: bigTree2()),
+
+          /// bird
+          Positioned(
+              top: value! * .20 + 160,
+              right: value! * .5 - 20,
+              child: birdDown()),
+
+          /// Grass
+          Positioned(
+              top: -value! * .75 + 345,
+              left: -value! * .2 - 5,
+              child: grass4()),
+          Positioned(
+              top: -value! * .80 + 360, left: 50, child: verySmallTree()),
+          Positioned(
+              top: -value! * .85 + 390,
+              right: -value! * .2 - 5,
+              child: grass3()),
+          Positioned(top: -value! * .85 + 380, right: 10, child: tree3()),
+          Positioned(
+              top: -value! * .95 + 435,
+              left: -value! * .2 - 5,
+              child: grass2()),
+          Positioned(
+              top: -value! * .98 + 420,
+              left: -value! * .5 - 10,
+              child: verySmallTreeR()),
+          Positioned(
+              top: -value! * .92 + 410, right: 100, child: verySmallTreeR()),
+          Positioned(
+              top: -value! * .98 + 460,
+              right: -value! * .5 - 10,
+              child: grass1()),
+          Positioned(
+              top: -value! * .98 + 455,
+              left: -value! * .5 - 10,
+              child: grass1()),
+          Positioned(
+              top: -value! + 475, left: -value! * .2 - 10, child: grass()),
+        ],
       ),
     );
   }
